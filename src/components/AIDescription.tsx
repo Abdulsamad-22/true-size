5c5937ab142569c83724ad260c3d7d1f946d8178
abdulsamad <abdulsamadhussaini001@gmail.com>
2026-02-07 15:47:29 +0100

    added a regenerate feature where description is not clear enough

--- src/components/AIDescription.tsx
+++ src/components/AIDescription.tsx
@@ -1,38 +1,109 @@
 "use client";
-import { ChevronUp, ChevronDown } from "lucide-react";
+import { ChevronUp, ChevronDown, RefreshCcw } from "lucide-react";
 import { useState } from "react";
+import { convert } from "../lib/units";
+import { DimensionId } from "../types";
+import { HistoryItem } from "../types";
+
+type SetHistory = React.Dispatch<React.SetStateAction<HistoryItem[]>>;
 
 interface descriptionProps {
   aiDescription: string;
   isLoadingAI: boolean;
+  setIsLoadingAI: (u: boolean) => void;
+  setAIDescription: (d: string) => void;
+  selectedUnit: string;
+  selectedDimension: DimensionId;
+  targetResult: string;
+  numericValue: number;
+  setHistory: SetHistory;
 }
 export default function AIDescription({
   aiDescription,
   isLoadingAI,
+  setIsLoadingAI,
+  setAIDescription,
+  selectedUnit,
+  selectedDimension,
+  targetResult,
+  numericValue,
+  setHistory,
 }: descriptionProps) {
   const [openDescription, setOpenDescription] = useState<boolean>(false);
   const handleDescription = () => {
-    setOpenDescription(!openDescription);
+    setOpenDescription((prev) => !prev);
   };
+
+  const handleRefreshDescription = async () => {
+    const { result, baseValue } = convert(
+      numericValue,
+      selectedUnit,
+      targetResult,
+      selectedDimension,
+    );
+    try {
+      setIsLoadingAI(true);
+
+      const res = await fetch("/api/generate", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          numericValue,
+          selectedUnit,
+          targetResult,
+          selectedDimension,
+          convertedValue: result,
+          baseValue,
+        }),
+      });
+
+      if (!res.ok) throw new Error("AI request failed");
+
+      const data = await res.json();
+      setAIDescription(data.text);
+
+      setHistory((prev) =>
+        prev.map((item) =>
+          item.id
+            ? {
+                ...item,
+                aiDescription: data.text,
+                timestamp: Date.now(),
+              }
+            : item,
+        ),
+      );
+    } catch (error) {
+      console.error("AI ERROR:", error);
+      setAIDescription("Unable to generate explanation right now.");
+    } finally {
+      setIsLoadingAI(false);
+    }
+  };
+  const Icon = openDescription ? ChevronUp : ChevronDown;
+
   return (
     <div className="border-[1px] border-[#C7D7F1] rounded-[12px] p-[1rem] mt-8">
-      <div className="flex item-center justify-between">
-        <h2 className="flex gap-2 text-[#212121] text-[1rem] font-medium mb-[1rem]">
+      <div className="flex items-center justify-between">
+        <div className="flex item-center gap-2">
           <div className="bg-[#338ADE] item-center py-[0.25rem] px-[0.5rem] rounded-[4px]">
             ✨
           </div>{" "}
-          AI Contextual Description
-        </h2>
+          <h2 className="text-[#212121] text-[1rem] font-medium">
+            AI Contextual Description
+          </h2>
+        </div>
 
-        {openDescription === false ? (
-          <ChevronDown onClick={handleDescription} size={20} />
-        ) : (
-          <ChevronUp onClick={handleDescription} size={20} />
-        )}
+        {<Icon onClick={handleDescription} size={20} />}
       </div>
 
       {openDescription && (
-        <div className="bg-[#338ADE] p-[0.625rem] rounded-[8px]">
+        <div className="bg-[#338ADE] p-[0.625rem] rounded-[8px] mt-[1rem]">
+          <RefreshCcw
+            onClick={handleRefreshDescription}
+            className="text-[#f3f3f3] mb-2"
+            size={20}
+          />
           {isLoadingAI && (
             <p className="text-sm text-[#f3f3f3] mt-2">
               Generating explanation…
